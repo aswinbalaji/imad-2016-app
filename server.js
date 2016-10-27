87e7a714306c9b3b3593d0fcbb7ea93e1c0f8763
aswinbalaji <aswinbalaji.r.2015.ece@rajalakshmi.edu.in>
2016-10-27 21:51:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var app = express();
+var app = express;
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/,article-one',function(req,res){
+   res.send('article one requested and will be served here'); 
+});
+app.get('/,article-two',function(req,res){
+   res.send('article two requested and will be served here'); 
+});
+app.get('/,article-three',function(req,res){
+   res.send('article three requested and will be served here'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
